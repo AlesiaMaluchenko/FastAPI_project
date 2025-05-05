33d40b9c6327759859d68bc247f56f9e68287577
AlesiaMaluchenko <maliuchenko.av@phystech.edu>
2025-05-05 03:49:42 +0300

    debug

--- test/test_database.sql
+++ test/test_database.sql
@@ -5,16 +5,14 @@ CREATE TABLE IF NOT EXISTS device (
 );
 
 CREATE TABLE IF NOT EXISTS article (
-    id TEXT PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS application (
     record_id INTEGER PRIMARY KEY AUTOINCREMENT,
     device_id INTEGER NOT NULL,
-    FOREIGN KEY (device_id) REFERENCES device (id),
     article_id INTEGER NOT NULL,
-    FOREIGN KEY (article_id) REFERENCES article (id),
     seq_obj TEXT NOT NULL
 );
 
