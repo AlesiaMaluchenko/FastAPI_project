9aaae4af4b09d0e1c5dc2c95df20f5dc71930abb
AlesiaMaluchenko <maliuchenko.av@phystech.edu>
2025-05-05 12:54:08 +0300

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,20 +1,21 @@
+from typing import Annotated
 import pydantic
 
 
 class SchemaIdentifier(pydantic.BaseModel):
-    id: int
+    id: Annotated[int, pydantic.Field(ge=0)]
 
 class SchemaArticle(pydantic.BaseModel):
-    id: int
+    id: Annotated[int, pydantic.Field(ge=0)]
     title: str
 
 class SchemaDevice(pydantic.BaseModel):
-    id: int
+    id: Annotated[int, pydantic.Field(ge=0)]
     name: str
     country: str
 
 class SchemaApplication(pydantic.BaseModel):
-    record_id: int
+    record_id: Annotated[int, pydantic.Field(ge=0)]
     device_id: str
     article_id: str
     seq_obj: str
