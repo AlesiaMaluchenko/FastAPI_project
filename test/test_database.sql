401e12d95dd80fe61354d5f1bac66a66b709ab00
AlesiaMaluchenko <maliuchenko.av@phystech.edu>
2025-05-05 03:18:36 +0300

    Update test_database.sql

--- test/test_database.sql
+++ test/test_database.sql
@@ -1,1 +1,19 @@
+CREATE TABLE IF NOT EXISTS device (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    country TEXT NOT NULL
+);
 
+CREATE TABLE IF NOT EXISTS article (
+    id TEXT PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS application (
+    record_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    device_id INTEGER NOT NULL,
+    FOREIGN KEY (device_id) REFERENCES device (id),
+    article_id INTEGER NOT NULL,
+    FOREIGN KEY (article_id) REFERENCES article (id),
+    seq_obj TEXT NOT NULL
+);
